04bcb80c722c04984c6212c6936f1d8fbe9180a5
songguo <970458312@qq.com>
2017-03-25 10:02:37 +0800

    注释信息，本次改变

---
+++ Hello.java
@@ -0,0 +1,17 @@
+package com.song.demo;
+
+import java.util.Date;
+
+/**
+ * Created by Administrator on 2017-03-25.
+ */
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+
+        Date date = new Date();
+
+
+    }
+}
